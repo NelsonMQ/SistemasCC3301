ef48c2ad65738ac30ee255a63b58e0ac6adcd958
NelsonMQ <nmarambi@dcc.uchile.cl>
2019-08-12 18:26:18 -0400

    Agregue nuevas funciones

--- Tarea1/t1.c
+++ Tarea1/t1.c
@@ -1,7 +1,6 @@
-
+#include <string.h>
 #include "t1.h"
 
-typedef unsigned int uint;
 uint comprimir(uint *a, int nbits){
     int mask = ~(-1<<nbits);
     uint *p = a;
@@ -16,3 +15,25 @@ uint comprimir(uint *a, int nbits){
     return u;
 }
 
+char *insercion(char *d, char *s){
+    char *newstr = malloc(strlen(d)+strlen(s)+1);
+    strcpy(newstr,s);
+    newstr = strcat(newstr,d);
+    return newstr;
+}
+
+void insertar(char *d, char *s){
+    char *p = d + strlen(d) + strlen(s);
+    char *p2 = d + strlen(d);
+    char *p3 = s + strlen(s) - 1;
+    for (int i = 0; i < strlen(d)+1; i++){
+        *p = *p2;
+        p--;
+        p2--;
+    }
+    for (int i = 0; i < strlen(s); i++){
+        *p = *p3;
+        p--;
+        p3--;
+    }
+}
