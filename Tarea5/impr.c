33766ad084db29eedfaf355f1ebaab8c6da8aee1
NelsonMQ <nmarambi@dcc.uchile.cl>
2019-10-12 19:50:01 -0300

    Tarea 5 agregada

---
+++ Tarea5/impr.c
@@ -0,0 +1,91 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "impr.h"
+
+/**
+ * Estructura para indicar la orden de imprimir un documento.
+ */
+struct orden {
+    Doc *doc;
+    int listo;
+};
+
+/**
+ * Definimos las variables globales
+ */
+
+ColaFifo *colaFifo;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_t t[8];
+
+/**
+ * Funcion que se ocupa en el thread y representa a una impresora
+ */
+void *t_imprimir(void *ptr){
+    Impr *impr = ptr;
+    for(;;) {
+        pthread_mutex_lock(&m);
+        while (vacia(colaFifo)) {
+            pthread_cond_wait(&cond, &m);
+        }
+        Orden *ord = extraer(colaFifo);
+        pthread_mutex_unlock(&m);
+        pthread_cond_broadcast(&cond);
+        imprimir(ord->doc, impr);
+        pthread_mutex_lock(&m);
+        ord->listo = 1;
+        pthread_mutex_unlock(&m);
+        pthread_cond_broadcast(&cond);
+        if(vacia(colaFifo)){
+            break;
+        }
+    }
+    return NULL;
+}
+
+/**
+ * Inicializa las impresoras
+ */
+void init_impr(Impr **imprs, int n_impr) {
+    colaFifo = nuevaColaFifo();
+    for(int i=0;i<n_impr;i++){
+        pthread_create(&t[i],NULL,t_imprimir,imprs[i]);
+    }
+}
+
+/**
+ * Entierra los threads que ocupaba cada impresora
+ */
+void terminar_impr() {
+    int i = 0;
+    for(;;){
+        if(pthread_join(t[i],NULL)!=0){
+            break;
+        }
+        i++;
+    }
+}
+
+/**
+ * Crea una orden de impresion para el documento
+ */
+Orden *imprimir_asinc(Doc *doc) {
+    Orden *ord = malloc(sizeof(Orden));
+    ord->doc=doc;
+    ord->listo=0;
+    agregar(colaFifo,ord);
+    pthread_cond_broadcast(&cond);
+    return ord;
+}
+/**
+ * Confirma que una orden este terminada
+ */
+void confirmar(Orden *ord) {
+    while(!ord->listo){
+        pthread_cond_wait(&cond,&m);
+    }
+    free(ord);
+}
